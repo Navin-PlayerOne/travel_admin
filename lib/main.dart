4892a0ee81cb6ccb18cbb303b8195dfa5c3dd7c8
Navin <goognav01@gmail.com>
2023-09-06 08:03:18 +0530

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:travel_admin/pages/home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return DynamicColorBuilder(
+      builder: (lightDynamic, darkDynamic) {
+        return MaterialApp(
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightDynamic ??
+                ColorScheme.fromSeed(
+                  brightness: Brightness.light,
+                  seedColor: Colors.red,
+                ),
+          ),
+          themeMode: ThemeMode.system,
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkDynamic ??
+                ColorScheme.fromSeed(
+                  brightness: Brightness.dark,
+                  seedColor: Colors.black,
+                ),
+          ),
+          routes: {
+            '/': (context) => const HomePage(),
+            // '/': (context) => AuthService().handleAuthState()
+          },
+        );
+      },
+    );
+  }
+}
